2af39f6a6975b932eb3e394f20ce40d9de88abe4
Jimmy Janssen <jjanssen@csmbp0010-jj.lan>
2019-10-11 08:29:26 -0400

    removing unused env vars

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { Server } from "./server";
 import { Vault } from "crds-vault-node";
 
 async function start() {
-    await new Vault(process.env.CRDS_ENV).process(['common', 'graphql', 'graphql-content']);
+    await new Vault(process.env.CRDS_ENV).process(['common', 'graphql']);
     return container.get<Server>(Types.Server).start();
 }
 
