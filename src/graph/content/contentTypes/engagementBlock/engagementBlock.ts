576644cdc1a8a8d6b039d8d5bd1a109ecfc29e27
janssejl <ll>
2021-03-15 14:23:51 -0400

    making content driver of engagements

--- src/graph/content/contentTypes/engagementBlock/engagementBlock.ts
+++ src/graph/content/contentTypes/engagementBlock/engagementBlock.ts
@@ -5,15 +5,18 @@ export default class EngagementBlock extends Content {
   public tooltipEarnedContent: string;
   public tooltipUnearnedContent: string;
   public disabled: boolean;
+  public type: string;
 
   constructor(entry) {
     super(entry);
 
     var fields = entry.fields;
     this.title = fields.title;
+    this.type = fields.type;
     this.targetUrl = fields.target_url;
     this.tooltipEarnedContent = fields.tooltip_earned_content;
     this.tooltipUnearnedContent = fields.tooltip_unearned_content;
     this.disabled = fields.disabled;
+    this.id = fields.slug;
   }
 }
